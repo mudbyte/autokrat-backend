21114ea4aa47cebe7657b7c0b68e6108eccfbed1
Don Kanaille <110260901+realdonkanaille@users.noreply.github.com>
2022-07-29 23:44:28 +0200

    Add spotless check (#2)

--- src/main/kotlin/de/mudbyte/de/autokrat/backend/AutokratBackendApplication.kt
+++ src/main/kotlin/de/mudbyte/de/autokrat/backend/AutokratBackendApplication.kt
@@ -3,9 +3,8 @@ package de.mudbyte.de.autokrat.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class AutokratBackendApplication
+@SpringBootApplication class AutokratBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<AutokratBackendApplication>(*args)
+  runApplication<AutokratBackendApplication>(*args)
 }
